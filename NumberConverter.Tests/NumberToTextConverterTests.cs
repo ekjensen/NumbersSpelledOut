2eede89a1aa6becfa660debb6dc4049c9393f9b9
Jeffrey Jensen <jensenj08@gmail.com>
2018-07-03 14:49:53 -0400

    Test that the number being returned is a good number.

--- NumberConverter.Tests/NumberToTextConverterTests.cs
+++ NumberConverter.Tests/NumberToTextConverterTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace NumberConverter.Tests
@@ -7,7 +8,7 @@ namespace NumberConverter.Tests
     public class NumberToTextConverterTests
     {
         [TestMethod]
-        public void Converts_thousands_properly()
+        public void Converts_thousands()
         {
             var numberConverter = new NumberToTextConverter(LetterCase.LowerCase);
 
@@ -19,6 +20,66 @@ namespace NumberConverter.Tests
         }
 
         [TestMethod]
+        public void Converts_hundreds()
+        {
+            var numberConverter = new NumberToTextConverter(LetterCase.LowerCase);
+
+            var oneHundred = numberConverter.GetText(100);
+            var nineHundred = numberConverter.GetText(900);
+            var twoHundredAndThree = numberConverter.GetText(203);
+            var threeHundredAndSeventySix = numberConverter.GetText(376);
+
+            Assert.AreEqual("one hundred", oneHundred);
+            Assert.AreEqual("nine hundred", nineHundred);
+            Assert.AreEqual("two hundred and three", twoHundredAndThree);
+            Assert.AreEqual("three hundred and seventy six", threeHundredAndSeventySix);
+        }
+
+        [TestMethod]
+        public void Converts_tens()
+        {
+            var numberConverter = new NumberToTextConverter(LetterCase.LowerCase);
+
+            var twenty = numberConverter.GetText(20);
+            var ninety = numberConverter.GetText(90);
+            var eightyEight = numberConverter.GetText(88);
+            var seventySeven = numberConverter.GetText(77);
+
+            Assert.AreEqual("twenty", twenty);
+            Assert.AreEqual("ninety", ninety);
+            Assert.AreEqual("eighty eight", eightyEight);
+            Assert.AreEqual("seventy seven", seventySeven);
+        }
+
+        [TestMethod]
+        public void Converts_teens()
+        {
+            var numberConverter = new NumberToTextConverter(LetterCase.LowerCase);
+
+            var ten = numberConverter.GetText(10);
+            var eleven = numberConverter.GetText(11);
+            var twelve = numberConverter.GetText(12);
+            var thirteen = numberConverter.GetText(13);
+            var fourteen = numberConverter.GetText(14);
+            var fifteen = numberConverter.GetText(15);
+            var sixteen = numberConverter.GetText(16);
+            var seventeen = numberConverter.GetText(17);
+            var eighteen = numberConverter.GetText(18);
+            var nineteen = numberConverter.GetText(19);
+
+            Assert.AreEqual("ten", ten);
+            Assert.AreEqual("eleven", eleven);
+            Assert.AreEqual("twelve", twelve);
+            Assert.AreEqual("thirteen", thirteen);
+            Assert.AreEqual("fourteen", fourteen);
+            Assert.AreEqual("fifteen", fifteen);
+            Assert.AreEqual("sixteen", sixteen);
+            Assert.AreEqual("seventeen", seventeen);
+            Assert.AreEqual("eighteen", eighteen);
+            Assert.AreEqual("nineteen", nineteen);
+        }
+
+        [TestMethod]
         public void Converts_full_number_properly()
         {
             var numberConverter = new NumberToTextConverter(LetterCase.LowerCase);
@@ -26,6 +87,48 @@ namespace NumberConverter.Tests
             var bigNumber = numberConverter.GetText(9876);
 
             Assert.AreEqual("nine thousand eight hundred and seventy six", bigNumber);
+        }
+
+        [TestMethod]
+        public void Uppercase_returns_upper_case_number()
+        {
+            var numberConverter = new NumberToTextConverter(LetterCase.UpperCase);
+
+            var bigNumber = numberConverter.GetText(9876);
+
+            Assert.AreEqual("NINE THOUSAND EIGHT HUNDRED AND SEVENTY SIX", bigNumber);
+        }
+
+        [TestMethod]
+        public void Title_text_returns_number_with_title_text()
+        {
+            var numberConverter = new NumberToTextConverter(LetterCase.TitleCase);
+
+            var bigNumber = numberConverter.GetText(9876);
+
+            Assert.AreEqual("Nine Thousand Eight Hundred and Seventy Six", bigNumber);
+        }
+
+        [TestMethod]
+        public void Zero_returns_zero()
+        {
+            var numberConverter = new NumberToTextConverter(LetterCase.LowerCase);
+
+            var zero = numberConverter.GetText(0);
+
+            Assert.AreEqual("zero", zero);
+        }
+
+        [TestMethod]
+        public void Seperator_contained_in_text()
+        {
+            var numberConverter = new NumberToTextConverter(LetterCase.LowerCase, "-");
+
+            var twentyThree = numberConverter.GetText(23);
+            var oneHundredFiftyFive = numberConverter.GetText(155);
+
+            Assert.AreEqual("twenty-three", twentyThree);
+            Assert.AreEqual("one-hundred-and-fifty-five", oneHundredFiftyFive);
         }
     }
 }
